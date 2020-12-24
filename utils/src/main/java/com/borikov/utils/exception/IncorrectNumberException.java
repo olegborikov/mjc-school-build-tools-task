c6003c7235f8e971601bdd03d71eb463a9a05664
Oleg Borikov <olegborikov999@gmail.com>
2020-12-24 10:52:10 +0300

    add StringUtils, add custom exception

---
+++ utils/src/main/java/com/borikov/utils/exception/IncorrectNumberException.java
@@ -0,0 +1,7 @@
+package com.borikov.utils.exception;
+
+public class IncorrectNumberException extends Exception{
+    public IncorrectNumberException(String message) {
+        super(message);
+    }
+}
---
+++ utils/src/main/java/com/borikov/utils/util/StringUtils.java
@@ -0,0 +1,17 @@
+package com.borikov.utils.util;
+
+import com.borikov.utils.exception.IncorrectNumberException;
+import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.math.NumberUtils;
+
+@UtilityClass
+public class StringUtils {
+    public boolean isPositiveNumber(String str) throws IncorrectNumberException {
+        if (NumberUtils.isCreatable(str)) {
+            double number = Double.parseDouble(str);
+            return number > 0;
+        } else {
+            throw new IncorrectNumberException("String doesn't contain a number: " + str);
+        }
+    }
+}
